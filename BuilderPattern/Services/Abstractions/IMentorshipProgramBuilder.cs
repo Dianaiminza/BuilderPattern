5588debf473910d076d98f7d49882eca7903787d
Captain <diana.iminza@ronforddigital.com>
2024-07-04 12:42:24 +0300

    add interface

---
+++ BuilderPattern/Services/Abstractions/IMentorshipProgramBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace BuilderPattern.Services.Abstractions
+{
+    public interface IMentorshipProgramBuilder
+    {
+        void SetProgramName(string name);
+        void SetDates(DateTime start, DateTime end);
+        void AddStudent(string student);
+        void AddMentor(string mentor);
+        void PairStudentWithMentor(string student, string mentor);
+        MentorshipProgram GetProgram();
+    }
+}
