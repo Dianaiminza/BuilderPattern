544e8b0eca5abf5ed5ae14da3412247108defd52
Captain <diana.iminza@ronforddigital.com>
2024-07-04 13:05:53 +0300

    program cs

--- BuilderPattern/Program.cs
+++ BuilderPattern/Program.cs
@@ -1,17 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using BuilderPattern.Services.Abstractions;
+using BuilderPattern.Services.Implementations;
+using BuilderPattern;
 
-// Add services to the container.
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMentorshipProgramBuilder builder = new MentorshipProgramBuilder();
+        MentorshipProgramDirector director = new MentorshipProgramDirector(builder);
 
-builder.Services.AddControllers();
+        var pairs = new List<(string student, string mentor)>
+        {
+            ("Alice", "Mentor1"),
+            ("Bob", "Mentor2"),
+            ("Captain", "Mentor3")
+        };
 
-var app = builder.Build();
+        director.ConstructProgram("STEM Mentorship Program", new DateTime(2024, 7, 1), new DateTime(2024, 12, 31), pairs);
+        MentorshipProgram program = director.GetProgram();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+        Console.WriteLine(program.ToString());
+    }
+}
--- BuilderPattern/Services/Implementations/MentorshipProgramBuilder.cs
+++ BuilderPattern/Services/Implementations/MentorshipProgramBuilder.cs
@@ -1,6 +1,46 @@
-﻿namespace BuilderPattern.Services.Implementations
+﻿using BuilderPattern.Services.Abstractions;
+
+namespace BuilderPattern.Services.Implementations
 {
-    public class MentorshipProgramBuilder
+    public class MentorshipProgramBuilder : IMentorshipProgramBuilder
     {
+        private MentorshipProgram _program;
+
+        public MentorshipProgramBuilder()
+        {
+            _program = new MentorshipProgram();
+        }
+
+        public void SetProgramName(string name)
+        {
+            _program.ProgramName = name;
+        }
+
+        public void SetDates(DateTime start, DateTime end)
+        {
+            _program.StartDate = start;
+            _program.EndDate = end;
+        }
+
+        public void AddStudent(string student)
+        {
+            _program.Students.Add(student);
+        }
+
+        public void AddMentor(string mentor)
+        {
+            _program.Mentors.Add(mentor);
+        }
+
+        public void PairStudentWithMentor(string student, string mentor)
+        {
+            _program.StudentMentorPairs[student] = mentor;
+        }
+
+        public MentorshipProgram GetProgram()
+        {
+            return _program;
+        }
     }
+
 }
